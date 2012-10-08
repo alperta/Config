063a7e0d780e5e7cfb33e5452120805b6f207fea
Alessandro Perta <alessandro.perta@gmail.com>
2012-10-08 18:33:53 +0300

    swapped boot and register methods

--- src/RedefineLab/Config/ConfigSilexServiceProvider.php
+++ src/RedefineLab/Config/ConfigSilexServiceProvider.php
@@ -7,14 +7,14 @@ class ConfigSilexServiceProvider implements \Silex\ServiceProviderInterface
 
     public function boot(\Silex\Application $app)
     {
-        $app['config'] = $app->share(function () use ($app) {
-            return new Config();
-        });
+        // Nothing to do here
     }
 
     public function register(\Silex\Application $app)
     {
-        // Nothing to do here
+        $app['config'] = $app->share(function () use ($app) {
+            return new Config();
+        });
     }
 
 }
